74c39e8a25cb854292f9f6964c7eaca7c7f3a236
Inge Bothma <ingesaunders@gmail.com>
2023-04-19 22:01:20 +0200

    changed nominal field to map over array

--- src/component/nominalChoiceField.jsx
+++ src/component/nominalChoiceField.jsx
@@ -1,30 +1,24 @@
 export default function NominalChoiceField(props) {
-  const nominalOption_1 = props.field.domain.values[0];
-  const nominalOption_2 = props.field.domain.values[1];
-  const nominalOption_3 = props.field.domain.values[2];
+  const nominalOptions = props.field.domain.values;
 
-  const changeHandler = (e) => props.setAnswer(props.field.name, e.target.value);
+  const changeHandler = (e) =>
+    props.setAnswer(props.field.name, e.target.value);
 
   return (
     <>
       <div className="nominal-choice-field">
         <div className="choice-field">
-        <input onChange={changeHandler} type="radio" value={nominalOption_1} />
-        <label>{nominalOption_1}</label>
-        </div>
-        <div className="choice-field">
-        <input onChange={changeHandler} type="radio" value={nominalOption_2} />
-        <label>{nominalOption_2}</label>
-        </div>
-        <div className="choice-field">
-        {nominalOption_3 ? (
-          <>
-            <input onChange={changeHandler}  type="radio" value={nominalOption_3} />
-            <label>{nominalOption_3}</label>
-          </>
-        ) : (
-          ""
-        )}
+          {nominalOptions.map((option) => (
+            <>
+              <input
+                onChange={changeHandler}
+                type="radio"
+                value={option}
+                name={props.field.name}
+              />
+              <label>{option}</label>
+            </>
+          ))}
         </div>
       </div>
     </>
