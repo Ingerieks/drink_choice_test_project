206e05969b215d724dba43a323c1961ec38ed23c
Inge Bothma <ingesaunders@gmail.com>
2023-04-20 10:34:27 +0200

    Render model name

--- src/component/questions.jsx
+++ src/component/questions.jsx
@@ -8,6 +8,7 @@ export default function Questionnaire() {
   const [questions, setQuestions] = useState([]);
   const [decision, setDecision] = useState("");
   const [answers, setAnswers] = useState({});
+  const [modelName, setModelName] = useState("");
 
   useEffect(() => {
     const url = "https://api.up2tom.com/v3/models/58d3bcf97c6b1644db73ad12";
@@ -19,6 +20,8 @@ export default function Questionnaire() {
     })
       .then((response) => response.json())
       .then((responseBody) => {
+        setModelName(responseBody.data.attributes.name)
+        //console.log(responseBody.data.attributes.name)
         setQuestions(responseBody.data.attributes.metadata.attributes);
       });
   }, []);
@@ -58,12 +61,12 @@ export default function Questionnaire() {
       });
   };
 
-  console.log(decision);
+  console.log(modelName);
 
   return (
     <div className="page">
       <form className="wrapper" onSubmit={handleSubmit}>
-        <h1 className="form-title">Drink choice questionnaire</h1>
+        <h1 className="form-title">{modelName}</h1>
         {questions.map((item) => (
           <div key={item.name} className="field-items">
             <Field setAnswer={setAnswer} field={item} />
